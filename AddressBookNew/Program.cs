13550c6bf0f2c26c44d72b101d7d6226a64d30ca
Lokendra <lokilokendra12345@gmail.com>
2020-10-18 20:01:08 +0530

    [lokendra] first commit

---
+++ AddressBookNew/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace adressBook
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+
+            contactBook firstContact = new contactBook("lokendra", "yeddula", "tpt", "AP", 533293, "9866256666", "xyz@abc.com");
+
+            firstContact.displayContact();
+        }
+    }
+}
